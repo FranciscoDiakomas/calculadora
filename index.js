45e46d76497adc515349aab84aae3134d1743fcc
Francisco Diakomas <fdiakomas@gmail.com>
2024-05-30 12:00:08 -0300

    melhorei em alguns aspectos no js

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-// @ts-nocheck
 window.onload = function () {
         let btns = document.querySelectorAll('button')
         btns.forEach((btn) => {
                 btn.addEventListener('click', (e) => {
                         let res = document.querySelector('.res')
                         if (btn.classList.contains('number') || btn.classList.contains('sinal')) {
-
-                                
-                                if (res.textContent == 'ERROR') {
-                                        res.textContent = e.target.textContent
-                                        
-                                } else {
-                                          res.textContent += e.target.textContent
-                                        
-                                }
+                                const mathError = document.getElementsByClassName("mathError")[0]
+                                mathError.textContent = "" 
+                                res.textContent += e.target.textContent
                                 const igualbtn = document.querySelector('.igual')
                                 igualbtn.addEventListener('click', () => {
-                                                if (String(res.textContent)[0]==='.') {
-                                                        res.textContent = 'ERROR'
-                                                        console.clear()
-                                                        return
-                                                } else {
+                                                try {
                                                         res.textContent = eval(res.textContent)
+                                        
+                                                }
+                                                catch (error) {
+                                                        res.textContent = ""
+                                                        const mathError = document.getElementsByClassName("mathError")[0]
+                                                        mathError.textContent = "Erro Matématico"
                                                 }
                                                 
-                                        }) 
-                               
+                                        })
                         } else if (e.target.textContent == 'Del') {
-                                 res.textContent = ''
+                                res.textContent = ''
                         }
-
-                     
-
-                       
                 })
         })
 }
